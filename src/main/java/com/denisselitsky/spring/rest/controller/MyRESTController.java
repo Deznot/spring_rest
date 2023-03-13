80f1074fc009fd63b6b1a08e375f7349f7f6fe60
Denis <denis.selitsky@gmail.com>
2023-03-13 11:08:27 +0300

    feat: add HandlerException methods

--- src/main/java/com/denisselitsky/spring/rest/controller/MyRESTController.java
+++ src/main/java/com/denisselitsky/spring/rest/controller/MyRESTController.java
@@ -1,12 +1,12 @@
 package com.denisselitsky.spring.rest.controller;
 
 import com.denisselitsky.spring.rest.entity.Employee;
+import com.denisselitsky.spring.rest.exception_handling.EmployeeIncorrectData;
+import com.denisselitsky.spring.rest.exception_handling.NoSuchEmployeeException;
 import com.denisselitsky.spring.rest.services.EmployeeService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
@@ -27,7 +27,11 @@ public class MyRESTController {
     @GetMapping("/employees/{id}")
     public Employee showEmployeeById(@PathVariable int id) {
         Employee employee = employeeService.getEmployee(id);
+
+        if(employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with id = " + id + " in Database");
+        }
+
         return employee;
     }
-
 }
